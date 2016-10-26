4fa40f3b1f39d6cc4515bb07df6f2d17ae853266
unknown <peterm@europa.hq.azuragroup.com>
2016-10-26 17:46:50 +0100

    Workaround for ember-cli issue 3718 (https://github.com/ember-cli/ember-cli/issues/3718).

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ module.exports = {
   name: 'ember-cli-uuid',
 
   included: function(app) {
+    if (app.constructor.name === 'CoreObject') {
+      while (!app.app && app.parent) {
+        app = app.parent;
+      }
+      app = app.app;
+    }
+    
     this._super.included(app);
 
-    this.app.import(app.bowerDirectory + '/node-uuid/uuid.js');
+    app.import(app.bowerDirectory + '/node-uuid/uuid.js');
   }
 
 };
